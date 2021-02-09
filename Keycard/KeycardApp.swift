21655d1636d5a8b8fc0534651bddf197ac709cf5
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-02-09 11:13:41 -0600

    Initial Commit

---
+++ Keycard/KeycardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeycardApp.swift
+//  Keycard
+//
+//  Created by Desiree on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct KeycardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
